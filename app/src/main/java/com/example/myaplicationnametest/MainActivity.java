2ca1eb6118403e3c2c6084e271664f30ecf0d972
Noodle96 <jorgealfredomarino2@gmail.com>
2020-02-01 11:41:03 -0500

    Context: OnCreate(), OnStart(), OnResume(), OnPause(), OnStop(), OnRestart(), OnDestroy().

--- app/src/main/java/com/example/myaplicationnametest/MainActivity.java
+++ app/src/main/java/com/example/myaplicationnametest/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Test.myToast(this);
         //Toast.makeText(this, "There is a new Actualization x2", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         int test = Test1();
     }
     //any to add
@@ -31,5 +32,41 @@ public class MainActivity extends AppCompatActivity {
             return a;
         }
         return b;
+    }
+    
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
