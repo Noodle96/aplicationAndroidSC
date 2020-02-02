214de4ff4c477320c1e56d45c7292fc58d5bd94b
Noodle96 <jorgealfredomarino2@gmail.com>
2020-02-02 01:03:04 -0500

    agregando librerias de terceros

--- app/src/main/java/com/example/myaplicationnametest/MainActivity.java
+++ app/src/main/java/com/example/myaplicationnametest/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
+import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         //Test.myToast(this);
         //Toast.makeText(this, "There is a new Actualization x2", Toast.LENGTH_LONG).show();
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
-        int test = Test1();
+        //Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        //int test = Test1();
+        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
+                .setButtonText("UNDO")
+                .setOnButtonClickListener("good_tag_name", null, null)
+                .setProgressBarColor(Color.WHITE)
+                .setText("Email deleted")
+                .setDuration(Style.DURATION_LONG)
+                .setFrame(Style.FRAME_LOLLIPOP)
+                .setColor(PaletteUtils.getSolidColor(PaletteUtils.MATERIAL_PURPLE))
+                .setAnimations(Style.ANIMATIONS_POP).show();
     }
     //any to add
     public int Test1(){
