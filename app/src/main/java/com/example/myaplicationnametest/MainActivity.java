ec27ef862bba9c8a5bb905c13c3d81ef0c7112e5
Noodle96 <jorgealfredomarino2@gmail.com>
2020-02-14 11:04:23 -0500

    agregando funcionalidad a los elementos UI x2 new

--- app/src/main/java/com/example/myaplicationnametest/MainActivity.java
+++ app/src/main/java/com/example/myaplicationnametest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.johnpersano.supertoasts.library.Style;
@@ -15,10 +16,19 @@ import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.buttonMain);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this,"button clicked from mainActivity!! althought codes", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //Activity activity = this;
         //Context context = this;
